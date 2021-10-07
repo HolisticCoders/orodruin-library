d22228c8ff4ef05cca426b4efe07eb16453bc617
Loïc Pinsard <muream@gmail.com>
2021-10-08 00:03:26 +0200

    Add QuaternionNormalize and TwistExtractor nodes

---
+++ maya/QuaternionNormalize.py
@@ -0,0 +1,31 @@
+from dataclasses import dataclass
+from typing import Dict
+from maya import cmds
+from orodruin_maya import OMNode
+
+@dataclass
+class QuaternionNormalize(OMNode):
+
+    def build(self):
+        self._input_node = cmds.createNode(
+            "quatNormalize",
+            name=self._name,
+        )
+
+        self._output_node = self._input_node
+
+    @staticmethod
+    def maya_attribute_map() -> Dict[str, str]:
+        """Return a dictionary mapping the ports names and their maya attributes."""
+        return {
+            "input": "inputQuat",
+            "inputX": "inputQuatX",
+            "inputY": "inputQuatY",
+            "inputZ": "inputQuatZ",
+            "inputW": "inputQuatW",
+            "output": "outputQuat",
+            "outputX": "outputQuatX",
+            "outputY": "outputQuatY",
+            "outputZ": "outputQuatZ",
+            "outputW": "outputQuatW",
+        }
