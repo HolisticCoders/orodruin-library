010dc82b2af178b63327b68f3252b2533f5dba16
Loïc Pinsard <loic@humain.co.uk>
2021-10-07 18:30:41 +0200

    Add Quaternion slerp node

---
+++ maya/QuaternionSlerp.py
@@ -0,0 +1,37 @@
+from dataclasses import dataclass
+from typing import Dict
+from maya import cmds
+from orodruin_maya import OMNode
+
+@dataclass
+class QuaternionSlerp(OMNode):
+
+    def build(self):
+        self._input_node = cmds.createNode(
+            "quatSlerp",
+            name=self._name,
+        )
+
+        self._output_node = self._input_node
+
+    @staticmethod
+    def maya_attribute_map() -> Dict[str, str]:
+        """Return a dictionary mapping the ports names and their maya attributes."""
+        return {
+            "input1": "input1Quat",
+            "input1X": "input1QuatX",
+            "input1Y": "input1QuatY",
+            "input1Z": "input1QuatZ",
+            "input1W": "input1QuatW",
+            "input2": "input2Quat",
+            "input2X": "input2QuatX",
+            "input2Y": "input2QuatY",
+            "input2Z": "input2QuatZ",
+            "input2W": "input2QuatW",
+            "T": "inputT",
+            "output": "outputQuat",
+            "outputX": "outputQuatX",
+            "outputY": "outputQuatY",
+            "outputZ": "outputQuatZ",
+            "outputW": "outputQuatW",
+        }
