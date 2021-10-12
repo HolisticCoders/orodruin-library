1d121fac3f0c65c4f54b34a5c50764c941b584d9
Loïc Pinsard <muream@gmail.com>
2021-10-12 23:54:59 +0200

    add RIG_IK node that encapsulate a two bones IK chain

---
+++ maya/QuaternionInverse.py
@@ -0,0 +1,33 @@
+from dataclasses import dataclass
+from typing import Dict
+
+from orodruin_maya import OMNode
+
+from maya import cmds
+
+
+@dataclass
+class QuaternionInverse(OMNode):
+    def build(self):
+        self._input_node = cmds.createNode(
+            "quatInvert",
+            name=self._name,
+        )
+
+        self._output_node = self._input_node
+
+    @staticmethod
+    def maya_attribute_map() -> Dict[str, str]:
+        """Return a dictionary mapping the ports names and their maya attributes."""
+        return {
+            "input": "inputQuat",
+            "inputX": "inputQuatX",
+            "inputY": "inputQuatY",
+            "inputZ": "inputQuatZ",
+            "inputW": "inputQuatW",
+            "output": "outputQuat",
+            "outputX": "outputQuatX",
+            "outputY": "outputQuatY",
+            "outputZ": "outputQuatZ",
+            "outputW": "outputQuatW",
+        }
