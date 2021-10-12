291649de5e3674c446f31cb6b1fd9b58163a3a2b
Loïc Pinsard <muream@gmail.com>
2021-10-12 23:54:23 +0200

    update the TwoBoneIK2D node to take bone lengths and controls as input

--- maya/TwoBoneIK2D.py
+++ maya/TwoBoneIK2D.py
@@ -7,32 +7,26 @@ from maya import cmds
 EXPRESSION_TEMPLATE = """
 float $pi = 3.141592654;
 
-vector $a = <<{0}.point_aX, {0}.point_aY, {0}.point_aZ>>;
-vector $b = <<{0}.point_bX, {0}.point_bY, {0}.point_bZ>>;
-vector $c = <<{0}.point_cX, {0}.point_cY, {0}.point_cZ>>;
+vector $base = <<{0}.baseX, {0}.baseY, {0}.baseZ>>;
 vector $handle = <<{0}.handleX, {0}.handleY, {0}.handleZ>>;
+float $bone_a_length = {0}.bone_a_length;
+float $bone_b_length = {0}.bone_b_length;
 
-vector $ab = $b - $a;
-vector $bc = $c - $b;
-vector $ac = $c - $a;
 
 // Compute the IK effector position.
-float $chain_length = mag($ab) + mag($bc);
-float $effector_vector_length = min($chain_length, mag($handle));
-vector $effector = unit($handle) * $effector_vector_length;
-vector $effector_vector = $effector - $a;
-
-float $ab_length = mag($ab);
-float $bc_length = mag($bc);
-float $effector_length = mag($effector_vector);
+vector $base_handle_vec = $handle - $base;
+float $chain_length = $bone_a_length + $bone_b_length;
+float $effector_vector_length = min($chain_length, mag($base_handle_vec));
+vector $effector = unit($base_handle_vec) * $effector_vector_length;
+float $effector_length = mag($effector);
 
 // Compute the angle of the "a" point
-float $cos_angle_a = ($ab_length * $ab_length + $effector_length * $effector_length - $bc_length * $bc_length) / (2 * $ab_length * $effector_length);
+float $cos_angle_a = ($bone_a_length * $bone_a_length + $effector_length * $effector_length - $bone_b_length * $bone_b_length) / (2 * $bone_a_length * $effector_length);
 $cos_angle_a = clamp(-1, 1, $cos_angle_a);
 float $angle_a = acos($cos_angle_a);
 
 // Compute the angle of the "b" point
-float $cos_angle_b = ($ab_length * $ab_length + $bc_length * $bc_length - $effector_length * $effector_length) / (2 * $ab_length * $bc_length);
+float $cos_angle_b = ($bone_a_length * $bone_a_length + $bone_b_length * $bone_b_length - $effector_length * $effector_length) / (2 * $bone_a_length * $bone_b_length);
 $cos_angle_b = clamp(-1, 1, $cos_angle_b);
 float $angle_b = acos($cos_angle_b);
 
@@ -49,8 +43,8 @@ class TwoBoneIK2D(OMGroupNode):
     def build(self) -> None:
         super().build()
 
-        # The creation of the expression nodes needs the maya attributes to exist
-        # so we create them before the orodruin ports are created.
+        # The orodruin ports are created after the call to `build` but the maya
+        # attributes need to exist before the creation of the expression node.
         self._create_maya_attributes()
 
         self._expression_template = EXPRESSION_TEMPLATE.format(
@@ -62,67 +56,24 @@ class TwoBoneIK2D(OMGroupNode):
         )
 
     def _create_maya_attributes(self) -> None:
-        cmds.addAttr(self._output_node, longName="angle_a", attributeType="double")
-        cmds.addAttr(self._output_node, longName="angle_b", attributeType="double")
-
-        cmds.addAttr(self._input_node, longName="point_a", attributeType="double3")
+        cmds.addAttr(self._input_node, longName="base", attributeType="double3")
         cmds.addAttr(
             self._input_node,
-            longName="point_aX",
+            longName="baseX",
             attributeType="double",
-            parent="point_a",
+            parent="base",
         )
         cmds.addAttr(
             self._input_node,
-            longName="point_aY",
+            longName="baseY",
             attributeType="double",
-            parent="point_a",
+            parent="base",
         )
         cmds.addAttr(
             self._input_node,
-            longName="point_aZ",
+            longName="baseZ",
             attributeType="double",
-            parent="point_a",
-        )
-
-        cmds.addAttr(self._input_node, longName="point_b", attributeType="double3")
-        cmds.addAttr(
-            self._input_node,
-            longName="point_bX",
-            attributeType="double",
-            parent="point_b",
-        )
-        cmds.addAttr(
-            self._input_node,
-            longName="point_bY",
-            attributeType="double",
-            parent="point_b",
-        )
-        cmds.addAttr(
-            self._input_node,
-            longName="point_bZ",
-            attributeType="double",
-            parent="point_b",
-        )
-
-        cmds.addAttr(self._input_node, longName="point_c", attributeType="double3")
-        cmds.addAttr(
-            self._input_node,
-            longName="point_cX",
-            attributeType="double",
-            parent="point_c",
-        )
-        cmds.addAttr(
-            self._input_node,
-            longName="point_cY",
-            attributeType="double",
-            parent="point_c",
-        )
-        cmds.addAttr(
-            self._input_node,
-            longName="point_cZ",
-            attributeType="double",
-            parent="point_c",
+            parent="base",
         )
 
         cmds.addAttr(self._input_node, longName="handle", attributeType="double3")
@@ -144,3 +95,9 @@ class TwoBoneIK2D(OMGroupNode):
             attributeType="double",
             parent="handle",
         )
+
+        cmds.addAttr(self._input_node, longName="bone_a_length", attributeType="double")
+        cmds.addAttr(self._input_node, longName="bone_b_length", attributeType="double")
+
+        cmds.addAttr(self._output_node, longName="angle_a", attributeType="double")
+        cmds.addAttr(self._output_node, longName="angle_b", attributeType="double")
---
+++ maya/VectorLength.py
@@ -0,0 +1,53 @@
+from dataclasses import dataclass, field
+
+from orodruin_maya import OMGroupNode
+
+from maya import cmds
+
+EXPRESSION_TEMPLATE = """
+vector $input = <<{0}.inputX, {0}.inputY, {0}.inputZ>>;
+{1}.output = mag($input);
+"""
+
+
+@dataclass
+class VectorLength(OMGroupNode):
+    _expression_template: str = field(init=False)
+    _expression_node: str = field(init=False)
+
+    def build(self) -> None:
+        super().build()
+
+        # The orodruin ports are created after the call to `build` but the maya
+        # attributes need to exist before the creation of the expression node.
+        self._create_maya_attributes()
+
+        self._expression_template = EXPRESSION_TEMPLATE.format(
+            self._input_node, self._output_node
+        )
+        self._expression_node = cmds.expression(
+            string=self._expression_template,
+            name=self.__class__.__name__ + "_EXP",
+        )
+
+    def _create_maya_attributes(self) -> None:
+        cmds.addAttr(self._input_node, longName="input", attributeType="double3")
+        cmds.addAttr(
+            self._input_node,
+            longName="inputX",
+            attributeType="double",
+            parent="input",
+        )
+        cmds.addAttr(
+            self._input_node,
+            longName="inputY",
+            attributeType="double",
+            parent="input",
+        )
+        cmds.addAttr(
+            self._input_node,
+            longName="inputZ",
+            attributeType="double",
+            parent="input",
+        )
+        cmds.addAttr(self._output_node, longName="output", attributeType="double")
